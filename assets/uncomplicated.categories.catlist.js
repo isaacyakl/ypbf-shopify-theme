cea69341bd4804184373d6a37e4b509b12438b7a
Isaac "Yak" L <webmaster@izacktheyak.com>
2017-01-31 18:15:52 -0800

    Add Related Products & Improve Product Page Template

--- assets/uncomplicated.categories.catlist.js
+++ assets/uncomplicated.categories.catlist.js
@@ -146,21 +146,6 @@
             }
         },
 
-    // untested, unused
-        hashList = function (list) {
-            var hash = {};
-            for (var i = 0; i < list.length; ++i) {
-                var item = list[i];
-                // create an array bucket for each URL
-                if (hash[item.url] == undefined) {
-                    hash[item.url] = [];
-                }
-                // add the subtree to that bucket
-                hash[item.url][hash[item.url].length] = item.pointer;
-            }
-            return hash;
-        },
-
         searchList = function (needle, list) {
             // simple linear search for now
             var matches = linearSearchList(needle, list);
@@ -240,6 +225,22 @@
             return false;
         },
 
+    // untested, unused
+        hashList = function (list) {
+            var hash = {};
+            for (var i = 0; i < list.length; ++i) {
+                var item = list[i];
+                // create an array bucket for each URL
+                if (hash[item.url] == undefined) {
+                    hash[item.url] = [];
+                }
+                // add the subtree to that bucket
+                hash[item.url][hash[item.url].length] = item.pointer;
+            }
+            return hash;
+        },
+
+    // categories-specific functions
         processCategorySelect = function (element, active_categories) {
             // attach to the onchange event
             bind(element, 'change', function (e) {
